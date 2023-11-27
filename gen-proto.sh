0ab653eb1e9bba3739ca72cb4ea5f92a35a65251
nhpd <dmitry.klpv@gmail.com>
2023-11-27 17:41:58 +0400

    write gen-proto.sh with buf

--- gen-proto.sh
+++ gen-proto.sh
@@ -1,42 +1,51 @@
 #!/usr/bin/env bash
 
-rm -r ./proto
-cp -r ../neutron/proto ./proto
+ROOT=$(pwd)
+IN="./tmp"
+OUT="./src/generated"
+TEMPLATE="./buf.ts.gen.yaml"
 
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-proto_files=()
+mkdir $IN
 
-for dir in $proto_dirs; do
-  proto_files=("${proto_files[@]} $(find "${dir}" -maxdepth 1 -name '*.proto')")
-done
+# ===== base types ============
+npx buf generate buf.build/protocolbuffers/wellknowntypes --template $TEMPLATE --output $OUT/wellknowntypes
 
-npx pbjs \
-  -o ./src/generated/proto.cjs \
-  -t static-module \
-  --force-long \
-  --keep-case \
-  --no-create \
-  --path=./proto/ \
-  --path=./proto-thirdparty/ \
-  --root="@neutron-org/neutron" \
-  ${proto_files[@]}
+# ===== neutron ===============
+git clone https://github.com/neutron-org/neutron $IN/neutron
+cd $IN/neutron
+# TODO: checkout the new sdk47 version
+git checkout -d 1 update-sdk47
+cd $ROOT
+npx buf generate $IN/neutron/proto --template $TEMPLATE --output $OUT/neutron
+npx buf generate $IN/neutron/third_party/proto --template $TEMPLATE --output $OUT/neutron_thirdparty
 
-npx pbjs \
-  -o ./src/generated/proto.js \
-  -t static-module \
-  -w es6 \
-  --es6 \
-  --force-long \
-  --keep-case \
-  --no-create \
-  --path=./proto/ \
-  --path=./proto-thirdparty/ \
-  --root="@neutron-org/neutron" \
-  ${proto_files[@]}
+# ===== neutron admin-module ==
+git clone https://github.com/neutron-org/admin-module $IN/admin-module
+cd $IN/admin-module
+git checkout v1.0.0 -d 1
+cd $ROOT
+npx buf generate $IN/admin-module/proto --template $TEMPLATE --output $OUT/admin_module
 
-npx pbts \
-  -o ./src/generated/proto.d.ts \
-  ./src/generated/proto.js
+# ===== neutron cosmos-sdk ====
+git clone github.com/neutron-org/cosmos-sdk $IN/cosmos-sdk
+cd $IN/cosmos-sdk
+git checkout -d 1 v0.47.6-neutron
+cd $ROOT
+npx buf generate $IN/cosmos-sdk/proto --template $TEMPLATE --output $OUT/cosmos_sdk
 
-rm ./src/generated/proto.js
-rm -r ./proto
+# ===== ibc-go ================
+git clone github.com/cosmos/ibc-go $IN/ibc-go
+cd $IN/ibc-go
+git checkout -d 1 v7.3.1
+cd $ROOT
+npx buf generate $IN/ibc-go/proto --template $TEMPLATE --output $OUT/ibc_go
+
+# note: commented because we don't have pob for now
+# ===== block-sdk (pob) =======
+# git clone git@github.com:skip-mev/block-sdk.git
+# cd $IN/block-sdk
+# git checkout -d 1 v1.1.0
+# cd $ROOT
+# npx buf generate $IN/block-sdk/proto --template $TEMPLATE --output $OUT/block_sdk
+
+rm -rf $IN
