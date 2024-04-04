8390f22670cf8b191467fac40aa833cd87d5e7cb
Dmitry Kolupaev <dmitry.klpv@gmail.com>
2024-04-04 17:17:31 +0400

    feat: add slinky proto types

--- gen-proto.sh
+++ gen-proto.sh
@@ -30,4 +30,8 @@ npx buf generate $REPOS/ibc-go/proto --template $TEMPLATE --output $OUT/ibc_go -
 git clone git@github.com:skip-mev/block-sdk.git $REPOS/block-sdk --branch v1.1.0 --depth 1
 npx buf generate $REPOS/block-sdk/proto --template $TEMPLATE --output $OUT/block_sdk --include-imports
 
+# ===== slinky ================
+git clone git@github.com:skip-mev/slinky.git $REPOS/slinky --branch v0.3.1 --depth 1
+npx buf generate $REPOS/slinky/proto --template $TEMPLATE --output $OUT/slinky --include-imports
+
 rm -rf $REPOS
