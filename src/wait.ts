8adf7508060527b6c20ce67be0a015421bd02002
nhpd <dmitry.klpv@gmail.com>
2024-05-02 19:26:43 +0400

    fix wait

--- src/wait.ts
+++ src/wait.ts
@@ -1,4 +1,4 @@
-import cosmosclient from '@cosmos-client/core';
+import { StreamingSocket } from '@cosmjs/socket';
 
 global.WebSocket = require('ws');
 
@@ -18,31 +18,49 @@ export class BlockWaiter {
     // const socket = new StreamingSocket(wsUrl + path, 3000);
     // socket.connect();
     // socket.events.subscribe();
+    console.log('waitBlocks()');
 
     return new Promise((resolve, reject) => {
-      let ws = null;
+      let ws: StreamingSocket = null;
       const x = setTimeout(() => {
         if (ws != null) {
-          ws.unsubscribe();
+          ws.disconnect();
         }
         reject(new Error('waitBlocks: timeout'));
       }, timeout);
-      ws = cosmosclient.websocket.connect(this.url);
-      ws.next({
-        id: '1',
-        jsonrpc: '2.0',
-        method: 'subscribe',
-        params: ["tm.event='NewBlock'"],
-      });
-      ws.subscribe((res: cosmosclient.websocket.ResponseSchema) => {
-        if (Object.entries(res.result).length !== 0) {
-          n--;
-          if (n == 0) {
-            ws.unsubscribe();
-            clearTimeout(x);
-            resolve();
+      ws = new StreamingSocket(this.url, timeout);
+      ws.send(
+        JSON.stringify({
+          id: '1',
+          jsonrpc: '2.0',
+          method: 'subscribe',
+          params: ["tm.event='NewBlock'"],
+        }),
+      );
+      ws.events.subscribe({
+        next: (res) => {
+          console.log('socket.next()');
+          if (Object.entries(res.data).length !== 0) {
+            n--;
+            if (n == 0) {
+              ws.disconnect();
+              clearTimeout(x);
+              resolve();
+            }
           }
-        }
+        },
+        error: () => {
+          // TODO
+          ws.disconnect();
+          clearTimeout(x);
+          resolve();
+        },
+        complete: () => {
+          // TODO
+          ws.disconnect();
+          clearTimeout(x);
+          resolve();
+        },
       });
     });
   }
